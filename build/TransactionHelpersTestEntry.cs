bf621d5b6206408e6cba4e5f1b244af5d580bd3d
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-19 15:42:03 +0800

    Added testing

---
+++ build/TransactionHelpersTestEntry.cs
@@ -0,0 +1,30 @@
+﻿using Nuke.Common.IO;
+using Nuke.Common.Tools.DotNet;
+using NukeBuildHelpers;
+using NukeBuildHelpers.Attributes;
+using NukeBuildHelpers.Enums;
+using NukeBuildHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _build;
+
+public class TransactionHelpersTestEntry : AppTestEntry<Build>
+{
+    public override bool RunParallel => false;
+
+    public override RunsOnType RunsOn => RunsOnType.Ubuntu2204;
+
+    public override Type[] AppEntryTargets => [typeof(TransactionHelpersEntry)];
+
+    public override void Run()
+    {
+        DotNetTasks.DotNetClean(_ => _
+            .SetProject(NukeBuild.Solution.TransactionHelpers_UnitTest));
+        DotNetTasks.DotNetTest(_ => _
+            .SetProjectFile(NukeBuild.Solution.TransactionHelpers_UnitTest));
+    }
+}
