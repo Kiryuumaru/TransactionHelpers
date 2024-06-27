8c5a40f507079792ffb4883b76fdc544adbd57b8
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2024-06-27 12:57:18 +0800

    Updated CICD (#65)

--- build/Build.cs
+++ build/Build.cs
@@ -6,8 +6,15 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using NukeBuildHelpers;
+using NukeBuildHelpers.Common.Attributes;
+using NukeBuildHelpers.Common.Enums;
+using NukeBuildHelpers.Entry;
+using NukeBuildHelpers.Entry.Extensions;
+using NukeBuildHelpers.RunContext.Extensions;
+using NukeBuildHelpers.Runner.Abstraction;
 
 public class Build : BaseNukeBuildHelpers
 {
@@ -16,4 +23,72 @@ public class Build : BaseNukeBuildHelpers
     public override string[] EnvironmentBranches { get; } = ["prerelease", "master"];
 
     public override string MainEnvironmentBranch => "master";
+
+    [SecretVariable("NUGET_AUTH_TOKEN")]
+    readonly string NuGetAuthToken;
+
+    [SecretVariable("GITHUB_TOKEN")]
+    readonly string GithubToken;
+
+    TestEntry TransactionHelpersTest => _ => _
+        .AppId("transaction_helpers")
+        .RunnerOS(RunnerOS.Ubuntu2204)
+        .Execute(context =>
+        {
+            var projectPath = RootDirectory / "TransactionHelpers.UnitTest" / "TransactionHelpers.UnitTest.csproj";
+
+            DotNetTasks.DotNetClean(_ => _
+                .SetProject(projectPath));
+            DotNetTasks.DotNetTest(_ => _
+                .SetProjectFile(projectPath));
+        });
+
+    BuildEntry TransactionHelpersBuild => _ => _
+        .AppId("transaction_helpers")
+        .RunnerOS(RunnerOS.Ubuntu2204)
+        .Condition(true)
+        .Execute(context =>
+        {
+            var projectPath = RootDirectory / "TransactionHelpers" / "TransactionHelpers.csproj";
+            var version = "0.0.0";
+            var releaseNotes = "";
+            if (context.TryGetBumpContext(out var bumpContext))
+            {
+                version = bumpContext.AppVersion.Version.ToString();
+                releaseNotes = bumpContext.AppVersion.ReleaseNotes;
+            }
+            DotNetTasks.DotNetClean(_ => _
+                .SetProject(projectPath));
+            DotNetTasks.DotNetBuild(_ => _
+                .SetProjectFile(projectPath)
+                .SetConfiguration("Release"));
+            DotNetTasks.DotNetPack(_ => _
+                .SetProject(projectPath)
+                .SetConfiguration("Release")
+                .SetNoRestore(true)
+                .SetNoBuild(true)
+                .SetIncludeSymbols(true)
+                .SetSymbolPackageFormat("snupkg")
+                .SetVersion(version)
+                .SetPackageReleaseNotes(releaseNotes)
+                .SetOutputDirectory(OutputDirectory));
+        });
+
+    PublishEntry TransactionHelpersPublish => _ => _
+        .AppId("transaction_helpers")
+        .RunnerOS(RunnerOS.Ubuntu2204)
+        .Execute(context =>
+        {
+            if (context.RunType == RunType.Bump)
+            {
+                DotNetTasks.DotNetNuGetPush(_ => _
+                    .SetSource("https://nuget.pkg.github.com/kiryuumaru/index.json")
+                    .SetApiKey(GithubToken)
+                    .SetTargetPath(OutputDirectory / "**"));
+                DotNetTasks.DotNetNuGetPush(_ => _
+                    .SetSource("https://api.nuget.org/v3/index.json")
+                    .SetApiKey(NuGetAuthToken)
+                    .SetTargetPath(OutputDirectory / "**"));
+            }
+        });
 }
